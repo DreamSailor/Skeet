763905a31347b35a9b35454b5878c460afec5c71
jeffry Simpson <jeffrysimpson@gmail.com>
2015-05-21 12:03:26 -0700

    updated main file name

---
+++ Skeet/skeetMain.cpp
@@ -0,0 +1,31 @@
+/***********************************************************************
+* Program:
+*    project 1, Procedural Chess            
+*    Brother McCracken, CS165
+* Author:
+*    Jeffry Simpson, Sam Koontz
+* Summary: 
+*    Enter a brief description of your program here!  Please note that if
+*    you do not take the time to fill out this block, YOU WILL LOSE POINTS.
+*    Before you begin working, estimate the time you think it will
+*    take you to do the assignment and include it in this header block.
+*    Before you submit the assignment include the actual time it took.
+*
+*    Estimated:  0.0 hrs   
+*    Actual:     0.0 hrs
+*      Please describe briefly what was the most difficult part.
+************************************************************************/
+
+#include <cstdlib>
+
+//using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+   return 0;
+}
+
